d000d5994444cb042f730a20a7d4cc7616d0a26b
KIMARI NELSON <kimari.nelson@mymona.uwi.edu>
2017-11-16 00:27:44 +0000

    final version

---
+++ world.js
@@ -0,0 +1,43 @@
+/*global $*/
+/*global $name*/
+/*global $value*/
+$(document).ready(function()
+{
+    $('#lookup').click(function()
+    {
+        if($('input[type="checkbox"]').prop("checked")==true)
+        {
+             $name = $('input[type="checkbox"]').attr('name');
+             $value = $('input[type="checkbox"]').attr('value');
+            $.ajax
+            ({
+                type: 'GET',
+                url: "world.php",
+                data:
+                {
+                    country: "$name '=' $value"
+                },
+                success: function(data)
+                {
+                    $('#result').html(data);
+                }
+            });
+        }
+        else
+        {
+            $.ajax
+            ({
+               type: 'GET',
+               url: 'world.php',
+               data:
+               {
+                   country: $('#country').val()
+               },
+               success: function(data)
+               {
+                   $('#result').html(data);
+               }
+            });
+        }
+    });
+});
--- world.php
+++ world.php
@@ -1,18 +1,45 @@
 <?php
 
 $host = getenv('IP');
-$username = getenv('C9_USER');
+$username = 'C9_USER';
 $password = '';
 $dbname = 'world';
+$country = $_GET['country'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $conn->query("SELECT * FROM countries");
+if($country == "all-true")
+{
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt -> fetchAll(PDO::FETCH_ASSOC);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    echo '<ul>';
+    foreach ($results as $row) 
+    {
+      echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
 }
-echo '</ul>';
+elseif($country == "")
+{
+   echo "enter a country please and thanks";  
+}
+else
+{
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    
+    if(!empty($results))
+    {
+        echo '<ul>';
+        foreach($results as $row)
+        {
+            echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+        }
+        echo '</ul>';
+    }
+    else
+    {
+        echo "Record doesnt exist";
+    }
+}
